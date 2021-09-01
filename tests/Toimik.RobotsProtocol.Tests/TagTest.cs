8737765a1b8709b81b453070b6144aef39f57d82
nurhafiz <nurhafiz@hotmail.sg>
2021-09-01 09:00:15 +0800

    Test uncovered code

---
+++ tests/Toimik.RobotsProtocol.Tests/TagTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Toimik.RobotsProtocol.Tests
+{
+    using Xunit;
+
+    public class TagTest
+    {
+        [Theory]
+        [InlineData("all", null, null, "all")]
+        [InlineData("all", null, "bot", "bot: all")]
+        [InlineData("max-snippet", "100", null, "max-snippet: 100")]
+        [InlineData("max-snippet", "100", "bot", "bot: max-snippet: 100")]
+        public void InstantiateWithNullUserAgent(
+            string directive,
+            string value,
+            string userAgent,
+            string expectedText)
+        {
+            var tag = new Tag(
+                directive: directive,
+                value: value,
+                userAgent: userAgent);
+
+            var actualText = tag.ToString();
+            Assert.Equal(expectedText, actualText);
+        }
+    }
+}
